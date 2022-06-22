41697baf2d171bd1965766599448126a84721e81
benja421 <101402787+benja421@users.noreply.github.com>
2022-06-22 02:30:28 -0400

    Add files via upload

--- rest_veterinaria/urls.py
+++ rest_veterinaria/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_veterinaria.views import lista_mascotas, addMascota, modEliminarMascota
+from rest_veterinaria.views import addAtenciones, addComuna, lista_atenciones, lista_comuna, lista_mascotas, addMascota, modEliminarAtenciones, modEliminarComuna, modEliminarMascota
 from rest_veterinaria.viewsLogin import ini_user
 
 urlpatterns = [
@@ -7,9 +7,15 @@ urlpatterns = [
     path('addMascota/',addMascota,name="addMascota"),
     path('modEliminarMascota/<codigo>',modEliminarMascota,name="modEliminarMascota"), 
     path('ini_user/',ini_user,name="ini_user"),
+    path('lista_comuna/',lista_comuna,name="lista_comuna"),
+    path('addComuna/',addComuna,name="addComuna"),
+    path('modEliminarComuna/<codigo>',modEliminarComuna,name="modEliminarComuna"), 
+    path('lista_atenciones/',lista_atenciones,name="lista_atenciones"),
+    path('addAtenciones/',addAtenciones,name="addAtenciones"),
+    path('modEliminarAtenciones/<codigo>',modEliminarAtenciones,name="modEliminarAtenciones"), 
 ]
-#lista listo
-#agregar falta test
-#modificar(put) falta test
+#lista(get) listo aa453698c726866340fc485b579891b5f4bbea05
+#agregar(post) listo
+#modificar(put) listo
 #eliminar listo
-#seguridad no implementado
+#seguridad listo
--- rest_veterinaria/views.py
+++ rest_veterinaria/views.py
@@ -7,16 +7,17 @@ from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from login.models import Mascota, Comuna, Atenciones
-from .serializers import MascotaSerializers
+from .serializers import MascotaSerializers, ComunaSerializers, AtencionesSerializers
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
+# Mascota
 @csrf_exempt
 @api_view(['GET','POST'])
-#@permission_classes((IsAuthenticated,)) remover el # una ves testiado todo
+@permission_classes((IsAuthenticated,))
 def lista_mascotas(request):
     if request.method == 'GET':
         mascota = Mascota.objects.all()
@@ -32,6 +33,7 @@ def lista_mascotas(request):
             return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
+@permission_classes((IsAuthenticated,))
 def addMascota(request):
     data = JSONParser().parse(request)
     serializer = MascotaSerializers(data = data)
@@ -42,7 +44,7 @@ def addMascota(request):
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
-
+@permission_classes((IsAuthenticated,))
 def modEliminarMascota(request,codigo):
     try:
         m = Mascota.objects.get(codigoChip = codigo)
@@ -66,3 +68,111 @@ def modEliminarMascota(request,codigo):
         m.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
     
+#Comuna
+@csrf_exempt
+@api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
+def lista_comuna(request):
+    if request.method == 'GET':
+        comuna = Comuna.objects.all()
+        serializador = ComunaSerializers(comuna, many=True)
+        return Response(serializador.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializador = ComunaSerializers(data = data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def addComuna(request):
+    data = JSONParser().parse(request)
+    serializer = ComunaSerializers(data = data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
+def modEliminarComuna(request,codigo):
+    try:
+        c = Comuna.objects.get(idComuna = codigo)
+    except Comuna.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ComunaSerializers(c)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data2 = JSONParser().parse(request)
+        serializer = ComunaSerializers(c, data = data2)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        c.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+#Atenciones
+
+@csrf_exempt
+@api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
+def lista_atenciones(request):
+    if request.method == 'GET':
+        atencion = Atenciones.objects.all()
+        serializador = AtencionesSerializers(atencion, many=True)
+        return Response(serializador.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializador = AtencionesSerializers(data = data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def addAtenciones(request):
+    data = JSONParser().parse(request)
+    serializer = AtencionesSerializers(data = data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
+def modEliminarAtenciones(request,codigo):
+    try:
+        a = Atenciones.objects.get(idComuna = codigo)
+    except Atenciones.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = AtencionesSerializers(a)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data2 = JSONParser().parse(request)
+        serializer = AtencionesSerializers(a, data = data2)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        a.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
